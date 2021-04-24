86216d08ed3c500ea95e8b86dc1e76bfeae2b759
Hinako-Tanaka <youngbird1228@gmail.com>
2021-04-24 16:34:42 +0900

    fourth commit

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Heading from './Heading';
 import './AboutSection.css';
+import {Link} from 'react-router-dom';
+import {Button} from './Button';
 
 function AboutSection() {
     return (
@@ -10,13 +12,18 @@ function AboutSection() {
             JapaneseHeading='私について' />
 
             <div className='about-wrapper'> 
-                <img src="http://placehold.jp/300x300.png" alt="" class="img-preview"></img>
+                <div className='about-img'>
+                    <img src="http://placehold.jp/300x300.png"></img>
+                </div>
                 <div className='about-text'>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius et esse doloribus non temporibus illum corrupti, officiis aperiam iste iure voluptate molestiae rem exercitationem suscipit vitae laudantium doloremque molestias vel?
                     </p>                              
                 </div>
             </div>
+            <Link to='/about'>
+                <Button buttonStyle='btn--outline' />
+            </Link>
         </section>
     )
 }
