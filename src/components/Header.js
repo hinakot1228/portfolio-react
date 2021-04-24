68cd91605134a8b8046e2613d3387bf8d320d1c1
Hinako-Tanaka <youngbird1228@gmail.com>
2021-04-24 13:11:57 +0900

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ function Header() {
             <header>
                 <nav className='navbar'>
                     <div className='navbar-container'>
-                        <Link to='/' className='navbar-logo'>
+                        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                             Hinako Tanaka<i class="fas fa-sun" />
                         </Link>
                         <div className='menu-icon' onClick={handleClick}>
