9e53a7ca3187f52c74b7390d835ba2b54e1afdf6
Hinako-Tanaka <youngbird1228@gmail.com>
2021-04-24 15:45:25 +0900

    third commit

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Heading from './Heading';
+import './AboutSection.css';
 
 function AboutSection() {
     return (
@@ -7,6 +8,15 @@ function AboutSection() {
             <Heading
             heading='ABOUT'
             JapaneseHeading='私について' />
+
+            <div className='about-wrapper'> 
+                <img src="http://placehold.jp/300x300.png" alt="" class="img-preview"></img>
+                <div className='about-text'>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius et esse doloribus non temporibus illum corrupti, officiis aperiam iste iure voluptate molestiae rem exercitationem suscipit vitae laudantium doloremque molestias vel?
+                    </p>                              
+                </div>
+            </div>
         </section>
     )
 }
