d1285351cc322a0a7280bdf90122210ae3f0e732
Hinako-Tanaka <youngbird1228@gmail.com>
2021-04-24 14:09:14 +0900

    initial commit

---
+++ src/components/AboutSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutSection() {
+    return (
+        <section className='about-section'>
+            <h2>ABOUT</h2>
+            <span>私について</span>
+        </section>
+    )
+}
+
+export default AboutSection
