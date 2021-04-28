cd6124196f7a27f5e6dddeffb2ad884e89527922
Hinako-Tanaka <youngbird1228@gmail.com>
2021-04-29 00:23:46 +0900

    skkills-section-modify

--- src/components/ProfileSection.js
+++ src/components/ProfileSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutHeading from './AboutHeading';
+import './ProfileSection.css';
 
 function ProfileSection() {
     return (
@@ -10,14 +11,21 @@ function ProfileSection() {
                 JapaneseHeading='プロフィール' />
 
                 <div className='profile-text'> 
-                    <p>テキストテキストテキストテキストテキスト</p>
-                    <p>テキストテキストテキストテキストテキスト</p>
-                    <p>テキストテキストテキストテキストテキスト</p>
-                    <p>テキストテキストテキストテキストテキスト</p>
+                    <p>
+                        千葉県出身の21歳。<br></br>
+                        大学4年で、大学では韓国語・英語、韓国の歴史や文化について勉強をしています。 <br></br>
+                        趣味は韓国映画やドラマを観ることです！
+                    </p>
+                    <p>
+                        2020年8月からプログラミングの独学を始め、10月からはプログラミングスクールのオンライン授業を受講し、HTML,CSS,PHP,Laravel,MySQLなどを学びました。
+                    </p>
+                    <p>
+                        現在は東京にある企業でエンジニアの長期インターンをしながら、日々プログラミングスキルを向上しています！
+                    </p>
                 </div>                
             </section>
         </>
     )
 }
 
-export default ProfileSection
+export default ProfileSection;
