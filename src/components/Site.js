0235f5099c9a84c0732c7b2ce69e2d1e6c7f248e
Hinako-Tanaka <youngbird1228@gmail.com>
2021-04-29 15:37:54 +0900

    second commit

--- src/components/Site.js
+++ src/components/Site.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import './Site.css';
 
-function Site(props) {
+class Site extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      text: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleChange(e){
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  handleClick(){
+    this.props.history.push({
+      pathname: "/product",
+      state: { text: this.state.text }
+  });
+  }
+
+  render() {
     return (
         <>
-            <div className='site-container'>
-                <div className='site-wrapper'>
-                    {/* <img className='site-img' src={`${process.env.PUBLIC_URL}/{props.image}`}></img> */}
-                    <img className='site-img' src={`${process.env.PUBLIC_URL}/website-2.png`}></img>
-                    <div className='site-info'>
-                        <span className='site-category'>
-                            {props.category}
-                        </span>
-                        <span className='site-name'>
-                            {props.name}
-                        </span>
-                    </div>
+        <div className='site-container'>
+            <div className='site-wrapper'>
+                {/* <img className='site-img' src={`${process.env.PUBLIC_URL}/{props.image}`}></img> */}
+                <img className='site-img' src={`${process.env.PUBLIC_URL}/website-2.png`}></img>
+                <div className='site-info'>
+                    <span className='site-category'>
+                        {this.state.category}
+                    </span>
+                    <span className='site-name'>
+                        {this.state.name}
+                    </span>
+                    <button onClick={this.handleClick}>画面遷移します</button>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    </>
+    );
+  }
 }
 
-export default Site;
+export default withRouter(Site)
