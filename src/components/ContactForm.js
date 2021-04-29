cdec97342b6690052a89f638209e2ebda9808139
Hinako-Tanaka <youngbird1228@gmail.com>
2021-04-29 16:45:49 +0900

    initial commit

---
+++ src/components/ContactForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ContactForm.css'
+
+function ContactForm() {
+    return (
+        <>
+            <div className='contact-container'>
+                <span>
+                    TwitterのDMからお問い合わせくださいくださいませ。24時間以内に返信いたします！
+                </span> 
+                <a href='https://twitter.com/ohinako1228' className='contact-btn'>
+                    <i class="fab fa-twitter-square"></i>
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default ContactForm
